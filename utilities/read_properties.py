311ffc7c5b7a5f6c7f759d9650fb9d0c9bb8d77c
Hana-Genut <0583220339hana@gmail.com>
2022-02-20 22:54:45 +0200

    path

--- utilities/read_properties.py
+++ utilities/read_properties.py
@@ -2,5 +2,5 @@ import xml.etree.ElementTree as ET
 
 
 def get_data(node_name):
-    root = ET.parse(r'C:\Automation\python_automation\configurations\config.xml').getroot()
+    root = ET.parse(r'configurations\config.xml').getroot()
     return root.find(".//" + node_name).text
